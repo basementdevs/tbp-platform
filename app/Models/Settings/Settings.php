b2e6a95cc3580a1acc29e172997fb16863adcfb1
danielhe4rt <hey@danielheart.dev>
2024-08-19 13:06:41 -0300

    wip: lint

--- app/Models/Settings/Settings.php
+++ app/Models/Settings/Settings.php
@@ -24,7 +24,6 @@ class Settings extends Model
         'is_developer' => 'boolean',
     ];
 
-
     public function getPronounsAttribute(): array
     {
         return config('extension.pronouns.'.$this->attributes['pronouns']);

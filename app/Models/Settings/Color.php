7e3103698468c3ecc2f7afc5b9be9d4466274489
danielhe4rt <hey@danielheart.dev>
2024-08-25 16:35:25 -0300

    fix: pipeline

--- app/Models/Settings/Color.php
+++ app/Models/Settings/Color.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Settings;
 
+use Database\Factories\ColorFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -22,5 +23,10 @@ class Color extends Model
     public function settings(): HasMany
     {
         return $this->hasMany(Settings::class);
+    }
+
+    protected static function newFactory(): ColorFactory
+    {
+        return ColorFactory::new();
     }
 }

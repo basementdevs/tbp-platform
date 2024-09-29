9377d8d34cc491ec9de8d24815379589f119f8b1
Gabriel do Carmo Vieira <48625433+gvieira18@users.noreply.github.com>
2024-09-28 23:29:35 -0300

    fix: resolve artisan name

--- app/Console/Commands/GenerateEffectSheet.php
+++ app/Console/Commands/GenerateEffectSheet.php
@@ -3,8 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Models\Settings\Effect;
-use Artisan;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 
 class GenerateEffectSheet extends Command
 {
